34075283323dea05da516e0c37a51051838e3b54
panchamipai21 <panchamipai21@gmail.com>
2025-02-27 16:55:52 +0530

    Added requirements.txt file

--- data_coll.py
+++ data_coll.py
@@ -1,38 +1,19 @@
-import os
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import numpy as np
+data_dict = pickle.load(open('./data.pickle','rb'))
+data = np.asarray(data_dict['data'])
+labels = np.asarray(data_dict['labels'])
 
-import cv2
+x_train, x_test, y_train, y_test = train_test_split(data,labels,test_size=0.2,shuffle=True,stratify=labels)
+model = RandomForestClassifier()
+model.fit(x_train, y_train)
+y_predict = model.predict(x_test)
+score = accuracy_score(y_predict, y_test)
+print('{}% of samples were classified correctly!'.format(score*100))
 
-
-DATA_DIR = './data'
-if not os.path.exists(DATA_DIR):
-    os.makedirs(DATA_DIR)
-
-number_of_classes = 26
-dataset_size = 100
-
-cap = cv2.VideoCapture(0)
-for j in range(number_of_classes):
-    if not os.path.exists(os.path.join(DATA_DIR, str(j))):
-        os.makedirs(os.path.join(DATA_DIR, str(j)))
-
-    print('Collecting data for class {}'.format(j))
-
-    done = False
-    while True:
-        ret, frame = cap.read()
-        cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,cv2.LINE_AA)
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(25) == ord('q'):
-            break
-
-    counter = 0
-    while counter < dataset_size:
-        ret, frame = cap.read()
-        cv2.imshow('frame', frame)
-        cv2.waitKey(25)
-        cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
-
-        counter += 1
-
-cap.release()
-cv2.destroyAllWindows()
+f = open('model.p', 'wb')
+pickle.dump({'model': model,}, f)
+f.close()
